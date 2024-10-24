f8ce33104ba4a8e5b900d0df7fb5d08205baeeb1
MaksimHamko <112936523+MaksimHamko@users.noreply.github.com>
2024-10-24 13:22:44 +0200

    Removing admin auth for registration and added userName in order to be able to register

--- GirafAPI/Entities/Users/DTOs/CreateUserDTO.cs
+++ GirafAPI/Entities/Users/DTOs/CreateUserDTO.cs
@@ -18,4 +18,7 @@ public record CreateUserDTO
     
     [StringLength(50)]
     public required string LastName { get; set; }
+    
+    [StringLength(100)]
+    public string? UserName { get; set; } 
 }
