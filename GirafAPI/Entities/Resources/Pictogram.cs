9ba9108ac33a60a4ac9d555accc87a72a2a972cb
Ronald Johnson <ronaldhej@gmail.com>
2024-10-29 01:40:02 +0100

    Added pictogram fields and CreatePictogramDTO

--- GirafAPI/Entities/Resources/Pictogram.cs
+++ GirafAPI/Entities/Resources/Pictogram.cs
@@ -3,5 +3,11 @@
 // Data model of pictograms in the database
 public class Pictogram
 {
+    public int Id { get; set; }
     
+    public Guid ImageId { get; set; }
+    
+    public string PictogramName { get; set; }
+    
+    //TODO: Add org when changes are merged
 }
