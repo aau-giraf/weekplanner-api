89fec8abd058748775a50520bf2d469147685f1c
Ronald Johnson <ronaldhej@gmail.com>
2024-11-04 01:57:41 +0100

    Added getbyorganisation endpoint, added organization fields to dtos and pictogram model, added fileutil for getting a IFormFile by filepath

---
+++ GirafAPI/Entities/Pictograms/Pictogram.cs
@@ -0,0 +1,10 @@
+﻿namespace GirafAPI.Entities.Pictograms;
+
+// Data model of pictograms in the database
+public class Pictogram
+{
+    public int Id { get; set; }
+    public int OrganizationId { get; set; }
+    public required Guid ImageId { get; set; }
+    public required string PictogramName { get; set; }
+}
