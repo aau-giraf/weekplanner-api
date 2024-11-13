8d53c1ad6e40698462b4570ba1cfd7efd86670da
Loke S. G. Jørgensen <lsgj21@student.aau.dk>
2024-11-13 11:56:11 +0100

    Requirement for user to be a member of the organization they're accessing

---
+++ GirafAPI/Authorization/OrgMemberRequirement.cs
@@ -0,0 +1,39 @@
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+
+namespace GirafAPI.Authorization;
+
+public class OrgMemberAuthorizationHandler : AuthorizationHandler<OrgMemberRequirement>
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+
+    public OrgMemberAuthorizationHandler(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+    }
+
+    protected override Task HandleRequirementAsync(
+        AuthorizationHandlerContext context,
+        OrgMemberRequirement requirement)
+    {
+        var claims = context.User;
+        var orgIds = claims.Claims
+                                .Where(c => c.Type == "OrgId")
+                                .Select(c => c.Value)
+                                .ToList();
+        
+        var httpContext = _httpContextAccessor.HttpContext;
+        var orgIdInUrl = httpContext.Request.RouteValues["orgId"];
+
+        if (orgIds.Contains(orgIdInUrl))
+        {
+            context.Succeed(requirement);
+            return Task.CompletedTask;
+        }
+        
+        context.Fail();
+        return Task.CompletedTask;
+    }
+}
+
+public class OrgMemberRequirement : IAuthorizationRequirement;
