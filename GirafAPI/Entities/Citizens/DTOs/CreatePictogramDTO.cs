22f40717061286041cf7966022089264da29ff38
Ronald Johnson <ronaldhej@gmail.com>
2024-11-14 20:12:27 +0100

    Deleted accidental pictogram dtos from wrong folder

--- GirafAPI/Entities/Citizens/DTOs/CreatePictogramDTO.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace GirafAPI.Entities.Pictograms.DTOs;
-
-public record CreatePictogramDTO(
-    [Required] IFormFile Image,
-    [Required] int OrganizationId,
-    [Required] string PictogramName
-    );
