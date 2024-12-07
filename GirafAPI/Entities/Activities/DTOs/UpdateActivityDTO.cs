30cd58f566c10c0b46c9a2bd21f86e67462d81c2
Ronald Johnson <ronaldhej@gmail.com>
2024-12-07 14:09:15 +0100

    Reverted Date and Time to strings, updated mapping and tests to reflect the changes

--- GirafAPI/Entities/Activities/DTOs/UpdateActivityDTO.cs
+++ GirafAPI/Entities/Activities/DTOs/UpdateActivityDTO.cs
@@ -5,9 +5,9 @@ namespace GirafAPI.Entities.Activities.DTOs;
 public record UpdateActivityDTO
 (
     [Required] int CitizenId,
-    [Required]DateOnly Date,
-    [Required]TimeOnly StartTime,
-    [Required]TimeOnly EndTime,
+    [Required][StringLength(10)] string Date,
+    [Required][StringLength(10)] string StartTime,
+    [Required][StringLength(10)] string EndTime,
     [Required] bool IsCompleted,
     int? PictogramId
 );
